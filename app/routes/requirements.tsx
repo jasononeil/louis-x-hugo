c895b67e00ee01750b75cc3a786013c72e5e0992
Rohan Nelson <rohan.nelson@gmail.com>
2024-05-13 12:02:25 +0800

    Added Common Options functionality

--- app/routes/requirements.tsx
+++ app/routes/requirements.tsx
@@ -13,6 +13,18 @@ export function HydrateFallback() {
   return <p>Loading...</p>;
 }
 
+const commonOptions = [
+  "School",
+  "Grandparents",
+  "Therapy",
+  "Playground",
+  "Daycare",
+  "Church",
+  "example",
+  "test",
+  "anotherTest",
+];
+
 export default function Requirements() {
   const pageData = useLoaderData<typeof clientLoader>();
   const emptyMagnet = () => ({ name: "", quantity: 0 });
@@ -25,6 +37,7 @@ export default function Requirements() {
     if (newMagnets[newMagnets.length - 1].name !== "") {
       newMagnets.push(emptyMagnet());
     }
+
     setMagnets(newMagnets);
     page.set({
       ...pageData,
@@ -32,17 +45,15 @@ export default function Requirements() {
     });
   }
 
-  const commonOptions = [
-    "School",
-    "Grandparents",
-    "Therapy",
-    "Playground",
-    "Daycare",
-    "Church",
-    "example",
-    "test",
-    "anotherTest",
-  ];
+  function clickCommonOption(option: string) {
+    updateMagnet(option, magnets.length - 1);
+  }
+
+  const filteredCommonOptions = commonOptions.filter((option) => {
+    return !magnets
+      .map((m) => m.name.toLowerCase())
+      .includes(option.toLowerCase());
+  });
 
   return (
     <div className="flex flex-col space-y-2 m-4">
@@ -59,9 +70,12 @@ export default function Requirements() {
         </ul>
         <h2 className="text-2xl font-bold">Common options</h2>
         <ul className="flex flex-wrap gap-4">
-          {commonOptions.map((option) => (
+          {filteredCommonOptions.map((option) => (
             <li>
-              <button className="p-2 border border-solid border-black rounded-xl">
+              <button
+                onClick={() => clickCommonOption(option)}
+                className="p-2 border border-solid border-black rounded-xl"
+              >
                 {option}
               </button>
             </li>
