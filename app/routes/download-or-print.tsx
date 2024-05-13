c7cf390d4e4b61a90e6882e75ef289c65df2b8bd
Jason O'Neil <jason.oneil@gmail.com>
2024-05-13 14:39:39 +0800

    Add magnet details to the details section on download page
    (We haven't got them rendering in a PDF yet)

--- app/routes/download-or-print.tsx
+++ app/routes/download-or-print.tsx
@@ -5,23 +5,34 @@ import { getDownloadUrl } from "./get-download-url";
 
 /** The client side loader, which runs after hydrate. Data from the serverLoader (`loader()`) is also available. */
 export async function clientLoader() {
-  const { name, weekStart, background } = page.get();
+  const { name, weekStart, background, magnets } = page.get();
   // TODO : once we have access to `pageData.background` on the server, we can use a server side `loader()` (and maybe `await serverLoader()` here) combined
   // with `getSignedUrlForItem()` rather than doing a separate fetch here. Then `get-download-url` might not even be needed. But that would require a
   // database or something 🤷
   const presignedBackground =
     background && (await getDownloadUrl({ key: background })).preSignedGetUrl;
 
+  const presignedMagnets = await Promise.all(
+    magnets.map(async (m) => ({
+      ...m,
+      presignedUrl: m.uploadKey
+        ? (await getDownloadUrl({ key: m.uploadKey })).preSignedGetUrl
+        : null,
+    }))
+  );
+
   return {
     name,
     weekStart,
     background: presignedBackground,
+    magnets: presignedMagnets,
   };
 }
 clientLoader.hydrate = true;
 
 export default function DownloadOrPrint() {
-  const { name, weekStart, background } = useLoaderData<typeof clientLoader>();
+  const { name, weekStart, background, magnets } =
+    useLoaderData<typeof clientLoader>();
   const params = new URLSearchParams({
     pageTitle: name || "Weekly Plan",
     firstDayOfWeek: weekStart || "sunday",
@@ -44,6 +55,19 @@ export default function DownloadOrPrint() {
             ) : (
               "No background"
             )}
+          </li>
+          <li>
+            Magnets:
+            <ul>
+              {magnets.map((magnet) => (
+                <li key={magnet.id}>
+                  {magnet.name}, {magnet.quantity}, {magnet.uploadKey}
+                  {magnet.presignedUrl && (
+                    <img src={magnet.presignedUrl} alt="" />
+                  )}
+                </li>
+              ))}
+            </ul>
           </li>
         </ul>
       </details>
